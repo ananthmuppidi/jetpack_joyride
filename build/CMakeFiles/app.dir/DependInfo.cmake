158a2ceff4187ea96506a65243601cebb2242b41
Ananth Muppidi <ananth.muppidi21@gmail.com>
2023-02-06 23:05:27 +0530

    Added basic breakout Classes.

---
+++ build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/game.cpp" "CMakeFiles/app.dir/src/game.cpp.o" "gcc" "CMakeFiles/app.dir/src/game.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/game_level.cpp" "CMakeFiles/app.dir/src/game_level.cpp.o" "gcc" "CMakeFiles/app.dir/src/game_level.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/game_object.cpp" "CMakeFiles/app.dir/src/game_object.cpp.o" "gcc" "CMakeFiles/app.dir/src/game_object.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/main.cpp" "CMakeFiles/app.dir/src/main.cpp.o" "gcc" "CMakeFiles/app.dir/src/main.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/resource_manager.cpp" "CMakeFiles/app.dir/src/resource_manager.cpp.o" "gcc" "CMakeFiles/app.dir/src/resource_manager.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/shader.cpp" "CMakeFiles/app.dir/src/shader.cpp.o" "gcc" "CMakeFiles/app.dir/src/shader.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/sprite_renderer.cpp" "CMakeFiles/app.dir/src/sprite_renderer.cpp.o" "gcc" "CMakeFiles/app.dir/src/sprite_renderer.cpp.o.d"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/src/texture.cpp" "CMakeFiles/app.dir/src/texture.cpp.o" "gcc" "CMakeFiles/app.dir/src/texture.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/ananthmuppidi/IIIT H/Semester 2/DSA/jetpack_joyride/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
